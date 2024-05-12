2c4677f8c92e552648f8cd64eb0f6b7e108ed4bb
eygrhuijo <joao555pepsg@hotmail.com>
2024-05-13 05:46:00 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-function canPartition(nums) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % 2 !== 0) return false;
-  const target = sum / 2;
-  const dp = new Array(target + 1).fill(false);
-  dp[0] = true;
-  for (const num of nums) {
-    for (let i = target; i >= num; i--) {
-      dp[i] = dp[i] || dp[i - num];
-    }
-  }
-  return dp[target];
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
+
+console.log(isPalindrome("racecar")); // true
