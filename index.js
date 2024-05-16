959e3c4b4a0aa2574b36432eac4189f2caf4a9df
eygrhuijo <joao555pepsg@hotmail.com>
2024-05-17 05:51:04 +0700

    update commit

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
+const dropRight = (arr, n = 1) => arr.slice(0, -n);
